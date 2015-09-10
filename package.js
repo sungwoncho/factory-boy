197db5dcb3231f130a6428b5280efb47cd5fd2c5
Sung Won Cho <mikeswcho@gmail.com>
2015-09-10 11:38:14 +1000

    Initial Commit

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'sungwoncho:factory-boy',
+  version: '0.0.1',
+  summary: 'Meteor factory to build documents for tests',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+  api.use('underscore');
+  api.addFiles('lib/factory-boy.js');
+
+  api.export('FactoryBoy');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('sungwoncho:factory-boy');
+  api.addFiles('tests/factory-boy-tests.js');
+});
